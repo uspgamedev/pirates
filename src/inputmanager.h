f9e194a31d18ec1d54eae446447b21f55327ba3d
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2011-07-20 15:08:36 -0300

    Wil: changed the tabs for spaces at inputmanager.h.

--- src/inputmanager.h
+++ src/inputmanager.h
@@ -3,26 +3,40 @@
 
 #include <cstdlib>
 #include "eventhandler.h"
+#include "game.h"
 
 class InputManager {
-private:
-	InputManager() {
-		//window->enable_keyboard;
-	}	
-	static InputManager* reference_;
-	static void ClickDownEvent(const Event* e, void *data){}
-	static void ClickUpEvent(const Event* e, void *data){}
 
-public:
-	static InputManager* reference(){
-		if (reference_ == NULL)
-			return (reference_ = new InputManager());
-		else
-			return reference_;
-	}
-	~InputManager() {
-		reference_ = NULL;
-	}
+  public:
+    
+    ~InputManager() {
+        reference_ = NULL;
+    }
+
+    static InputManager* reference() {
+        if (reference_ == NULL)
+            return (reference_ = new InputManager());
+        else
+            return reference_;
+    }
+
+    void Init () {
+        Game::reference()->window()->enable_keyboard();
+    }
+
+  private:
+
+    InputManager() :  {}
+
+    Game*& game_;
+
+    static InputManager* reference_;
+
+    static void ClickDownEvent(const Event* e, void *data) {}
+
+    static void ClickUpEvent(const Event* e, void *data) {}
+
 };
 
 #endif
+

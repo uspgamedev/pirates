bc34ff6433c6a183a0c9187468af9987899166a3
Gorobaum <gorobaum@hotmail.com>
2011-07-26 09:36:29 -0300

    Putting the boat to move

--- src/boat.cpp
+++ src/boat.cpp
@@ -12,4 +12,24 @@ Boat::Boat (std::string &modelpath, WindowFramework*& window, PandaFramework& fr
     boat_node_ = m;
 }
 
+AsyncTask::DoneStatus Boat::moveBoat ( GenericAsyncTask* task, void* data ) {
+  double time = globalClock_->get_real_time();
+  double angledegrees = time * 6.0;
+  double angleradians = angledegrees * (3.14 / 180.0);
+  boat_node_.set_pos(20*sin(angleradians),-20.0*cos(angleradians),3);
+  boat_node_.set_hpr(angledegrees, 0, 0);
+ 
+  return AsyncTask::DS_cont;
+}
+
+void Boat::taskInicialize() {
+    PT(AsyncTaskManager) taskMgr = AsyncTaskManager::get_global_ptr();
+    PT(ClockObject) globalClock = ClockObject::get_global_clock();
+    taskMgr_ = taskMgr;
+    globalClock_ = globalClock;
+    taskMgr_->add(new GenericAsyncTask("Ainda nao sei",
+        &moveBoat, (void*) NULL));
+}
+
+
 }
--- src/boat.h
+++ src/boat.h
@@ -8,6 +8,8 @@
 #define BOAT_H_
 
 #include "pandaFramework.h"
+#include "genericAsyncTask.h"
+#include "asyncTaskManager.h"
 #include "lpoint3.h"
 #include <string>
 
@@ -19,10 +21,12 @@ class Boat {
    
     Boat (std::string &modelpath, WindowFramework*& window, PandaFramework& framework );
     LPoint3f node_pos () { return boat_node_.get_pos(); }
-
+    AsyncTask::DoneStatus moveBoat ( GenericAsyncTask* task, void* data );
+    void taskInicialize();
     
   private :
-
+    PT(AsyncTaskManager) taskMgr_;
+    PT(ClockObject) globalClock_;
     NodePath boat_node_;
 };
 
